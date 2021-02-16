7cc1c6b54b59ceffd023dd1b694e86e572cb8ac2
Baphe02 <77804270+Baphe02@users.noreply.github.com>
2021-02-16 21:45:31 +0100

    Update and rename PozdravnaMerged - končna to Pozdravna000.java

--- Pozdravna000.java
+++ Pozdravna000.java
@@ -1,4 +1,3 @@
-## pozdravna
 import java.util.HashMap;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -18,25 +17,33 @@ import java.awt.Font;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 
+/*
+ * Okno namenjeno 'prihodu' v program. Uporabnik ima na voljo 2 možnosti. Če nima 'računa', ga lahko ustvari. Če ga ima, se lahko samo prijavi.
+ * Ob pritiski na ustrezen gumb se zažene ustrezno okno.
+ */
 public class Pozdravna implements ActionListener {
     JFrame frame;
     JButton registracijaKnof;
     JButton prijavaKnof;
     JLabel pozdrav;
-    BufferedImage img;
-    
+
+   BufferedImage img;
+
     public Pozdravna() {
         frame = new JFrame("Pozdravna stran!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //da se zapre ko klikns x
         frame.setSize(500,500);                                 //velikost okvirja
         frame.getContentPane().setBackground(new Color(160,165,230));
+        frame.setResizable(false);                              //okno je fiksne velikosti
         frame.setResizable(false);
 
         ImageIcon ikona = new ImageIcon("sovica03.png"); //ustvari ikono 
         frame.setIconImage(ikona.getImage());   // ikonca frame-a
         ImageIcon sovica = new ImageIcon("sovica03.png"); 
-        
-        /*# pozdrav - (!) poravnava, da je vse na sredni */ 
+
+ /** pozdravni message */ 
+
+  /*# pozdrav - (!) poravnava, da je vse na sredni */ 
         JLabel pozdrav = new JLabel("Pozdravljen v Sovicah!"); //JLabel.CENTER     
         //pozdrav.setVerticalAlignment(JLabel.CENTER);
         //pozdrav.setHorizontalAlignment(JLabel.CENTER);
@@ -45,27 +52,21 @@ public class Pozdravna implements ActionListener {
         pozdrav.setFont(new Font("Comic Sans",Font.ITALIC, 25));
         pozdrav.setBounds(100, 100, 300, 50);
 
-        /** slika (!) dodat **/
-        //sovica.setBounds(100, 175, 300, 250));
-
-        // slika sovice
-        //
-
-        /** knofi **/
-       registracijaKnof = new JButton("Registracija");
+ //sovica.setBounds(100, 175, 300, 250));
+ /** knofi **/
+registracijaKnof = new JButton("Registracija");
 
         registracijaKnof.setBounds(50, 350, 200, 50);
         registracijaKnof.setFocusable(false);
         registracijaKnof.addActionListener(this);
-
         prijavaKnof = new JButton("Prijava");
         prijavaKnof.setBounds(255, 350, 200, 50);
         prijavaKnof.setFocusable(false);
         prijavaKnof.addActionListener(this);
-        
+
         JLabel image = new JLabel();
         image.setBounds(150, 150, 200, 175);
-        
+
         img = null;
         try {
             img = ImageIO.read(new File("sovica03.png"));
@@ -76,33 +77,29 @@ public class Pozdravna implements ActionListener {
         Image dimg = img.getScaledInstance(image.getWidth(), image.getHeight(), Image.SCALE_SMOOTH);
         ImageIcon imgIcon = new ImageIcon(dimg);
         image.setIcon(imgIcon);
-        
+
         /**------dodajanje na frame---------**/
-        
+
         frame.add(image);
         frame.add(pozdrav); 
         frame.add(registracijaKnof);
         frame.add(prijavaKnof); 
 
         frame.setLocationRelativeTo(null);                      //odpre na sredini
-        frame.setLayout(null);                                 
-        //frame.pack();
-        frame.setVisible(true);     
+  frame.setLayout(null);                                         
+        frame.setVisible(true);                                 //da se prižge
 
     }
 
     public void actionPerformed(ActionEvent e) {
-        
+
         if(e.getSource() == registracijaKnof) {
-           RegisterPage reg = new RegisterPage();   //odpremo RegisterPage
-        }
+            RegisterPage reg = new RegisterPage();               //odpremo RegisterPage
+    }
 
         if(e.getSource() == prijavaKnof) {
-           frame.dispose(); 
-           
-           LoginPage log = new LoginPage(IDandPasswords.getLoginInfo());    //odpremo LoginPage in ji damo hashmap imen in gesel
-        }
+            frame.dispose(); 
+            LoginPage log = new LoginPage(IDandPasswords.getLoginInfo());    //odpremo LoginPage in ji damo hashmap imen in gesel
+  }
     }
 }
-    
-
