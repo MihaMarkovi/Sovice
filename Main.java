00abb13a0ebee89458495fcc33218a4ac0f1cdc8
aljazig <77804384+aljazig@users.noreply.github.com>
2021-02-16 12:23:53 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.ImageIcon;
+
+public class Main {
+    public static void main(String[] arggs) {
+       ImageIcon image = new ImageIcon("sovica03.png"); //ustvari ikono
+       IDandPasswords idandPasswords = new IDandPasswords();  
+       
+       
+       //Pozdravna poz = new Pozdravna();
+       Pozdravna poz = new Pozdravna();
+      // RegisterPage registerPage = new RegisterPage(idandPasswords.getLoginInfo());
+      //LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+    }
+}
